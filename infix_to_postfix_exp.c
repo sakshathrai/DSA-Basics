9e7e94af1e181e6eeec76fbeef0543e6c7432a9a
Sakshath Rai K <90779367+sakshathrai@users.noreply.github.com>
2023-03-13 01:24:20 +0530

    Update infix_to_postfix_exp.c

--- infix_to_postfix_exp.c
+++ infix_to_postfix_exp.c
@@ -2,21 +2,40 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int stack[10], top = -1, i = 0;
-char exp1[10];
-float res;
+#define STACK_SIZE 10
 
-void push(int val)
+float stack[STACK_SIZE];
+int top = -1;
+int i = 0;
+char exp1[10];
+
+void push(float val)
 {
-    top++;
-    stack[top] = val;
+    if (top < STACK_SIZE - 1)
+    {
+        top++;
+        stack[top] = val;
+    }
+    else
+    {
+        printf("Error: Stack overflow\n");
+        exit(1);
+    }
 }
 
-int pop()
+float pop()
 {
-    int val1 = stack[top];
-    top--;
-    return val1;
+    if (top >= 0)
+    {
+        float val1 = stack[top];
+        top--;
+        return val1;
+    }
+    else
+    {
+        printf("Error: Stack underflow\n");
+        exit(1);
+    }
 }
 
 float compute(float op1, float op2, char symbol)
@@ -25,42 +44,50 @@ float compute(float op1, float op2, char symbol)
     switch (symbol)
     {
     case '+':
-        res = (op1 - 48) + (op2 - 48);
+        res = op1 + op2;
         break;
-
     case '-':
-        res = (op1 - 48) - (op2 - 48);
+        res = op1 - op2;
         break;
-
     case '*':
-        res = (op1 - 48) * (op2 - 48);
+        res = op1 * op2;
         break;
-
     case '/':
-        res = (op1 - 48) / (op2 - 48);
+        if (op2 == 0)
+        {
+            printf("Error: Division by zero\n");
+            exit(1);
+        }
+        res = op1 / op2;
         break;
+    default:
+        printf("Error: Invalid operator\n");
+        exit(1);
     }
-    return res + 48;
+    return res;
 }
 
 int main()
 {
-    printf("Enter the expression in Infix: ");
+    printf("Enter the expression in Postfix: ");
     scanf("%s", exp1);
     while (exp1[i] != '\0')
     {
-        if (isdigit(exp1[i]))
-            push(exp1[i]);
+        if (isalnum(exp1[i]))
+        {
+            float val = exp1[i] - '0';
+            push(val);
+        }
         else
         {
             float op2 = pop();
             float op1 = pop();
             char symbol = exp1[i];
-            res = compute(op1, op2, symbol);
+            float res = compute(op1, op2, symbol);
             push(res);
         }
         i++;
     }
-    printf("Result is %f\n", res - 48);
+    printf("Result is %f\n", pop());
     return 0;
 }
